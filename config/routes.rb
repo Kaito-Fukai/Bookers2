2d85adbc33c63c65fe6cf08e98b85fc572b752f0
Kaito-Fukai <yaiag04@gmail.com>
2018-02-13 15:30:31 +0900

    before attaching users-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'bookers/top' => 'bookers#top'
 
   resources :bookers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'bookers#top'
 
 
 end
